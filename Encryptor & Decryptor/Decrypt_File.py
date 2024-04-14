3d9daf906d82cbff746cd0e8f26fbea05dae2a60
Aditya <aroo25may@gmail.com>
2024-04-14 21:58:30 +0530

    FINISHED (aisa muje lagta hai)

--- Encryptor & Decryptor/Decrypt_File.py
+++ Encryptor & Decryptor/Decrypt_File.py
@@ -1,10 +1,10 @@
 from cryptography.fernet import Fernet
 
-key = "cdT0ZnN7FdUS-WfDmzIvyN9zugMcYOcEyICSd3h8jsE="
+key = "VgwNiPId1ddoJ9PkkYxzXjHKpsWoV-7fCGoffzdskwI="
 
-key_information = "I_Key.txt"
-key_system_information = "S_Key.txt"
-key_clipboard_information = "C_Key.txt"
+key_information = "E_Key_Logs.txt"
+key_system_information = "E_System_Info.txt"
+key_clipboard_information = "E_Clipboard.txt"
 
 encrypted_files = [key_information, key_system_information, key_clipboard_information]
 count = 0
--- Encryptor & Decryptor/Generate_Key.py
+++ Encryptor & Decryptor/Generate_Key.py
@@ -1,6 +1,5 @@
 from cryptography.fernet import Fernet
 
-
 key = Fernet.generate_key()
 file = open("Encryptor & Decryptor\\Encryption_Key.txt", 'wb')
 file.write(key)
